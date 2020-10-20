17c9154cd514b6927a16bc83df1b2acf02ec2cba
quocanhvien99 <quocanhvien99@gmail.com>
2020-10-20 10:57:13 +0700

    .

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const corsOptions = {
 	origin: 'http://localhost:3000',
 	credentials: true,
 	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	allowedHeaders: 'Content-Type',
 	preflightContinue: false,
 	optionsSuccessStatus: 204
   }
