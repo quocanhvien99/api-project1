ff93ce3118073047deb831de9848a130a67002c9
quocanhvien99 <quocanhvien99@gmail.com>
2021-01-15 00:19:12 +0700

    15/1

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,42 @@
-const router = require("express").Router();
-const passport = require("passport");
-const CLIENT_HOME_PAGE_URL = "http://localhost:3000";
+const router = require('express').Router();
+const passport = require('passport');
 
 // When logout, redirect to client
-router.get("/logout", (req, res) => {
-  req.logout();
-  //res.redirect(CLIENT_HOME_PAGE_URL);
-  res.redirect(process.env.FRONTEND_URL);
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect(process.env.FRONTEND_URL);
 });
 
 // auth with google
-router.get("/google", passport.authenticate("google", {
-    scope: ['profile', 'email']
-}));
+router.get(
+	'/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email'],
+	})
+);
 
 // redirect to home page after successfully login via google
-router.get("/google/redirect",
-  passport.authenticate("google", {
-    successRedirect: process.env.FRONTEND_URL
-}));
+router.get(
+	'/google/redirect',
+	passport.authenticate('google', {
+		successRedirect: process.env.FRONTEND_URL,
+	})
+);
 
-router.get("/facebook", passport.authenticate("facebook"));
+router.get('/facebook', passport.authenticate('facebook'));
 
-router.get("/facebook/redirect",
-  passport.authenticate("facebook", {
-    successRedirect: process.env.FRONTEND_URL
-}));
+router.get(
+	'/facebook/redirect',
+	passport.authenticate('facebook', {
+		successRedirect: process.env.FRONTEND_URL,
+	})
+);
 
-router.post('/login', 
-  passport.authenticate('local', {
-    //successRedirect: CLIENT_HOME_PAGE_URL
-    successRedirect: process.env.FRONTEND_URL
-}));
+router.post(
+	'/login',
+	passport.authenticate('local', {
+		successRedirect: process.env.FRONTEND_URL,
+	})
+);
 
 module.exports = router;
